ace6b0a4a313a4a99d8efd40c98eabaadf4899ea
homura <homura.dev@gmail.com>
2020-02-13 23:03:09 +0800

    feat: supported config MUTA_CHAIN by env

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,15 @@ export const MUTA_ENDPOINT =
   process.env.MUTA_ENDPOINT || 'http://127.0.0.1:8000/graphql';
 
 export const MUTA_CHAINID =
+  process.env.MUTA_CHAINID ||
   '0xb6a4d7da21443f5e816e8700eea87610e6d769657d6b8ec73028457bf2ca4036';
 
 /**
  * max concurrency num for sync data from chain
  */
-export const SYNC_CONCURRENCY = 500;
+export const SYNC_CONCURRENCY: number = Number(
+  process.env.HERMIT_FETCH_CONCURRENCY ?? 500,
+);
 
 export const HERMIT_PORT = process.env.HERMIT_PORT || 4040;
 
