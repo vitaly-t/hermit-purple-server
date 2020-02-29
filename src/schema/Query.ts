ba74912c1a67982388ac42e9a0a859535dd04c79
homura <homura.dev@gmail.com>
2020-02-29 12:05:50 +0800

    perf: hack the transferHistories

--- src/schema/Query.ts
+++ src/schema/Query.ts
@@ -1,4 +1,5 @@
-import { queryType } from 'nexus';
+import { intArg, queryType, stringArg } from 'nexus';
+import { prisma } from '../sync';
 
 export const Query = queryType({
   definition(t) {
@@ -20,10 +21,65 @@ export const Query = queryType({
       filtering: true,
       pagination: true,
     });
-    t.crud.transferHistories({
-      ordering: true,
-      filtering: true,
-      pagination: true,
+
+    t.list.field('transferHistories', {
+      type: 'TransferHistory',
+      nullable: true,
+      args: {
+        from: stringArg({ description: 'from address' }),
+        to: stringArg({ description: 'to address' }),
+        assetId: stringArg({ description: '' }),
+        txHash: stringArg({ description: '' }),
+        blockHeight: intArg(),
+        first: intArg(),
+        last: intArg(),
+        skip: intArg(),
+      },
+      async resolve(root, args, ctx) {
+        const {
+          first,
+          last,
+          assetId,
+          blockHeight,
+          from,
+          txHash,
+          to,
+          skip,
+        } = args;
+        const limit = first || last;
+        const offset = skip ?? 0;
+        const orderBy = first ? 'ASC' : 'DESC';
+
+        let whereClause = Object.entries({
+          assetId,
+          blockHeight,
+          from,
+          to,
+          txHash,
+        })
+          .map(([key, value]) => value && `"${key}" = '${value}'`)
+          .filter(x => x)
+          .join(' AND ');
+        whereClause = whereClause && `where ${whereClause}`;
+        const sql = `
+              SELECT
+                *
+              from(
+                  select
+                    *
+                  from public."TransferHistory"
+                  ${whereClause}
+                  ORDER BY
+                    "id" DESC
+                ) as "items"
+              ORDER BY
+                "id" ${orderBy}
+              offset
+                ${offset}
+              limit
+                ${limit};`;
+        return await prisma.raw(sql);
+      },
     });
   },
 });
