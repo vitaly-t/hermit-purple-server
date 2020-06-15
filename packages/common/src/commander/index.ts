8f271fd0c30f18838d64a52c040581b8fd132d17
homura <homura.dev@gmail.com>
2020-06-15 23:50:18 +0800

    feat(common): named  createVersionedCommander return type

--- packages/common/src/commander/index.ts
+++ packages/common/src/commander/index.ts
@@ -19,7 +19,7 @@ function callerDir(depth: number = 1): string {
 export function createVersionedCommander(
   cwd: string = callerDir(2),
   name?: string,
-) {
+): Command {
   loadEnvFile();
   const program = new Command(name);
   program.version(readPkgUp.sync({ cwd })?.packageJson.version ?? 'unknown');
