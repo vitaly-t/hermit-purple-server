58c192e2f9d9527f1afa2ac8445e6f7bcb0eb3df
homura <homura.dev@gmail.com>
2020-06-21 20:09:18 +0800

    feat(knex-mysql):support read configuration first

--- packages/knex-mysql/src/bin/index.ts
+++ packages/knex-mysql/src/bin/index.ts
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+require('@muta-extra/common').loadEnvFile();
+
 import { Migration1591797537928 } from '../migration/Migration1591797537928';
 import { createRunnableMigrate } from '../migration/run';
 
